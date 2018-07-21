473c5b9102f72b374af9d5b5e4f7cd60c86c80ee
Heather Downing <Heather.Downing@vml.com>
2018-07-21 14:15:22 -0500

    Basic send and recieve SMS loop created

---
+++ NotificationService/SugarNotificationAPI/SugarNotificationAPI/Controllers/SmsUserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Twilio.AspNet.Mvc;
+using Twilio.TwiML;
+
+namespace SugarNotificationAPI.Controllers
+{
+    public class SmsUserController : TwilioController
+    {
+        // GET: SmsUser
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Sms/Message
+        [HttpPost]
+        public ActionResult Message(string From, string Body)
+        {
+            var twiml = new MessagingResponse();
+            var message = twiml.Message($"Hello {From}. You said {Body}");
+            return TwiML(message);
+        }
+    }
+}
